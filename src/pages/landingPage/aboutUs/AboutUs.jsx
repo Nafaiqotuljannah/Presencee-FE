0053900f7d72bcd6b0f47f0b4dc125c5d094e695
unknown <vikiade00@gmail.com>
2023-05-24 17:58:11 +0700

    feat/landing-page : Update About Us Fix

--- src/pages/landingPage/aboutUs/AboutUs.jsx
+++ src/pages/landingPage/aboutUs/AboutUs.jsx
@@ -7,17 +7,14 @@ const AboutUs = () => {
     <>
       <div className="about-section">
         <div className="about">
+          <img src={IMAGES.logo1} alt="about us image" />
           <div className="about-text">
-            <h1>PRESENSEE</h1>
             <p>
               Presensee merupakan platform untuk presensi berbasis online.
               Presensee hadir untuk memudahkan akses dalam mengisi absensi dari
               segi peserta didik dan membantu pendidikan mengetahui kehadiran
               dari peserta didik.
             </p>
-          </div>
-          <div className="about-image">
-            <img src={IMAGES.AboutUs} alt="about us image" width={500} />
           </div>
         </div>
       </div>
