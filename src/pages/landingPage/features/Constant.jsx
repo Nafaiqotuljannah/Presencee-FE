0e492614b12a2b0c7142933edb8d9ac8ba452f52
Rezkianwar <123230737+Rezkianwar@users.noreply.github.com>
2023-05-24 21:29:18 +0700

    Create Constant.jsx
    membuat file constant.jsx untuk diimpor ke file feature.jsx

---
+++ src/pages/landingPage/features/Constant.jsx
@@ -0,0 +1,47 @@
+import { IMAGES } from "../../../assets/constant";
+
+export const FEATURES_CONTENT = [
+  {
+    id: 1,
+    bgImage: IMAGES.bgFeatures,
+    title: "Presensi Sidik Jari",
+  },
+  {
+    id: 2,
+    bgImage: IMAGES.bgFeatures2,
+    title: "Jadwal",
+  },
+  {
+    id: 3,
+    bgImage: IMAGES.bgFeatures3,
+    title: "Histori Presensi",
+  },
+  {
+    id: 4,
+    bgImage: IMAGES.bgFeatures4,
+    title: "Pesensi kamera",
+  },
+];
+
+export const FAQ = [
+  {
+    id: 1,
+    bgimg: IMAGES.faq1,
+    description: "Apa itu absensi online?",
+  },
+  {
+    id: 2,
+    bgimg: IMAGES.faq2,
+    description: "Kenapa harus Presensee?",
+  },
+  {
+    id: 3,
+    bgimg: IMAGES.faq3,
+    description: "Bagaimana cara Presensee memvalidasi kehadiran?",
+  },
+  {
+    id: 4,
+    bgimg: IMAGES.faq4,
+    description: "Bagaimana jika universitas saya ingin bekerja sama dengan Presensee?",
+  },
+];
