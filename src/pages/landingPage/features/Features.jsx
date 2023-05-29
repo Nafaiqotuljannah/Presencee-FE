029a833cdfe5af141d43cf7e6e992f1090736d19
Rezkianwar <123230737+Rezkianwar@users.noreply.github.com>
2023-05-29 19:23:03 +0700

    feat/landing page: update pop up features di features.jsx

--- src/pages/landingPage/features/Features.jsx
+++ src/pages/landingPage/features/Features.jsx
@@ -1,9 +1,17 @@
 import React, { useState } from "react";
-import { Carousel, Col, Row } from "antd";
+import { Col, Row } from "antd";
 import "./Features.css";
 import { FAQ } from "./Constant";
 
 const Features = () => {
+  const [overlayVisible, setOverlayVisible] = useState([false, false, false, false]);
+
+  const handleContainerClick = (index) => {
+    const newOverlays = [...overlayVisible];
+    newOverlays[index] = !newOverlays[index];
+    setOverlayVisible(newOverlays);
+  };
+
   return (
     <>
       <div>
@@ -11,17 +19,37 @@ const Features = () => {
         <Row>
           <Col className="col1" span={3}></Col>
           <Col className="col2" xs={24} md={18} lg={18}>
-            <div className="text-container1">
+            <div className="text-container1" onClick={() => handleContainerClick(0)}>
               <p className="text">Presensi Sidik Jari</p>
+              {overlayVisible[0] && (
+                <div className="overlay">
+                  <p className="overlay-text">Dengan fitur ini, data yang terinput tidak dapat dipalsukan, karena pada dasarnya setiap manusia memiliki sidik jari yang berbeda.</p>
+                </div>
+              )}
             </div>
-            <div className="text-container2">
+            <div className="text-container2" onClick={() => handleContainerClick(1)}>
               <p className="text">Jadwal</p>
+              {overlayVisible[1] && (
+                <div className="overlay">
+                  <p className="overlay-text">Dengan fitur ini, dapat memudahkan dalam mengelola waktu sehingga tahu rencana kuliah yang harus dilakukan.</p>
+                </div>
+              )}
             </div>
-            <div className="text-container3">
+            <div className="text-container3" onClick={() => handleContainerClick(2)}>
               <p className="text">Histori Presensi</p>
+              {overlayVisible[2] && (
+                <div className="overlay">
+                  <p className="overlay-text">Dengan fitur ini, dapat memudahkan dalam melihat kembali atau meninjau riwayat absensi mahasiswa.</p>
+                </div>
+              )}
             </div>
-            <div className="text-container4">
+            <div className="text-container4" onClick={() => handleContainerClick(4)}>
               <p className="text">Pesensi kamera</p>
+              {overlayVisible[4] && (
+                <div className="overlay">
+                  <p className="overlay-text">Dengan fitur ini, absensi akan lebih efektif.</p>
+                </div>
+              )}
             </div>
           </Col>
           <Col className="col3" span={3}></Col>
